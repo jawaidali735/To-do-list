7cbbc1d38009667a2184b55aa489a266900b9f08
Jawaid Ali <jawaidaliupdates@gmail.com>
2024-04-01 02:56:32 +0500

    first programing of todolist

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env/node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let answers = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "list",
+            message: "What do you want to add to your todos?",
+            choices: ["Pray", "Eat", "Sleep", "Work", "Exercise", "Study", "Read", "Write", "Listen"]
+        },
+        {
+            name: "addMore",
+            type: "list",
+            message: "Do you want to add more to your todos?",
+            default: "No",
+            choices: ["Yes", "No"]
+        }
+    ]);
+    todos.push(answers.todo);
+    if (answers.addMore === "No") {
+        console.log("Thanks for using the todo list.");
+        condition = false;
+    }
+}
+console.log("Your todos:", todos);
